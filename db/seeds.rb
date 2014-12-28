497257febc5c30c6cef2de45a4dd88da4f357f09
alexrivadeneira <alexrivs@gmail.com>
2014-12-27 19:58:33 -0500

    test

---
+++ db/seeds.rb
@@ -0,0 +1,30 @@
+User.create({name: "Alex", password: }
+	])
+
+Concept.create([
+	{body: "Barack Obama"},
+	{body: "Facebook"},
+	{body: "Running"},
+	{body: "Sour Patch Kids"},
+	{body: "Classical Music"},
+	{body: "Children"},
+	{body: "Jewelry"},
+	{body: "Cooking"},
+	{body: "Microsoft"},
+	{body: "Abstract Art"},
+	{body: "Going to the Movies"},
+	{body: "Football"},
+	{body: "McDonalds"},
+	{body: "Tennis"},
+	{body: "Recycling"},
+	{body: "Country Music"},
+	{body: "Bill Gates"},
+	{body: "Ice Skating"},
+	{body: "Osama bin Laden"},
+	{body: "Boardgames"},
+	{body: "Harry Potter"},
+	{body: "Katie Couric"},
+	{body: "Biking"},
+	{body: "Hiking"},
+	{body: "Coffee"}
+	])
